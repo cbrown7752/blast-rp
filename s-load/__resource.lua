98dc4854e9c15fee3bef423a6b9fe4413158ba6b
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-07-18 23:09:37 -0500

    Add files via upload

---
+++ s-load/__resource.lua
@@ -0,0 +1,17 @@
+--[[
+This loading screen is made by me but with snippets from the following authors.
+ Kroxiox - Based on his loading screen.
+ Panzar - Text font and music snippet.
+ Star Animation - snippet taken from a old loadingscreen I found.
+--]]
+
+description 'Trolling_Seagulls | Loadingscreen'
+
+files {
+  'index.html',
+  'css/1.css',
+  'music/music.ogg',
+}
+
+loadscreen 'index.html'
+
