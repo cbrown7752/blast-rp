3a622bf9f0408ebbd767eb2293e230fe011cc7b5
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-07-29 13:59:38 -0500

    Update script.js

--- Resources/script.js
+++ Resources/script.js
@@ -1,37 +1,16 @@
-$(function() {
-	
-	// Auto-selection when clicked
-	$(".search").click(function() {
-  		$(this).select();
-		$(".search").css('color', 'black');
-	});
-	
-	// Text turns gray again when not typing
-	$(".search").blur(function() {
-		$(".search").css('color', '#7A7A7A');
-	});
-	
-	// Expand search bar
-	$(".search").focus(function() {
-		$(this).animate({ width: "300px" }, 250);
-	});
-	
-	$(".search").blur(function() {
-		$(this).animate({ width: "150px" }, 200);
-	});
-	
-	// Search... dissapears when field is clicked
-	$('.search').focus(function() {
-	  if ($(this).val() === 'Search...') {
-	    return $(this).val('');
-	  }
-	});
-	
-	// Search... reappears when field is not in focus anymore
-    $('.search').blur(function() {
-      if ($(this).val() === '') {
-        return $(this).val('Search...');
-      }
-	});
-	
-});
+
+// JavaScript code
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
